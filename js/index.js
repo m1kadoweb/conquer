51970ce9cb5a71bc747a8f924ce768168032e696
m1kadoweb <37939676+m1kadoweb@users.noreply.github.com>
2020-12-03 10:20:03 +0500

    fix: improve display transition to the anchors

--- js/index.js
+++ js/index.js
@@ -18,14 +18,14 @@ backToTopButton.addEventListener('click', backToTop);
 let anchors = document.querySelectorAll('.scroll-to');
 for(let anchor of anchors) {
     anchor.addEventListener('click', (e) => {
-        e.preventDefault;
+        e.preventDefault();
 
         const blockId = anchor.getAttribute('href');
-
         document.querySelector(blockId).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         })
+        
     })
 }
 
